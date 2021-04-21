53ada4d7a5b94a49f09bd2574fe49232f39329d0
James Kelley <jkkelley86@gmail.com>
2021-04-21 11:06:39 -0500

    first commit

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,47 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+// Checking to see our array is empty in the console below.
+console.log('Our basket array will return empty.', basket);
+
+function addItem( item ) {
+  basket.push(item);
+  // Using this expression below, we're showing that the basket has 1 item or more
+  // in it.
+  if (basket.length > 0) {
+    return true;
+  }
+}
+
+console.log('Let\'s add Salsa to our basket using addItem:', addItem('Salsa'));
+console.log('Returning our basket.', basket);
+
+
+function listItems(array) {
+  for (x = 0; x < array.length; x++) {
+    console.log(array[x]);
+  }
+  return array;
+}
+
+console.log('Let\'s test our listItems function:', listItems(basket));
+
+
+// Let's add some more items to our basket using our addItem function.
+addItem('Popcorn');
+addItem('Juice');
+addItem('Milk');
+addItem('Cheese');
+
+console.log('Let\'s see our basket now:', listItems(basket));
+
+addItem('Corn');
+addItem('Soda Pop');
+addItem('Soup');
+
+console.log('And one let\'s check our basket one more time:', listItems(basket));
+
+// console.log('Let\'s see our basket now:', listItems(basket));
